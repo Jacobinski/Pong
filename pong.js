ac0ad9ab0125626ce0cbd6be61c02de6cd6a2a44
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2017-09-20 23:52:43 -0600

    Change colors to Black & White

--- pong.js
+++ pong.js
@@ -25,7 +25,7 @@ function Paddle(x, y, width, height) {
 };
 
 Paddle.prototype.render = function() {
-    context.fillStyle = "#0000FF";
+    context.fillStyle = "#FFFFFF";
     context.fillRect(this.x, this.y, this.width, this.height);
 };
 
@@ -56,7 +56,7 @@ function Ball(x, y) {
 Ball.prototype.render = function() {
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
-    context.fillStyle = "#000000";
+    context.fillStyle = "#FFFFFF";
     context.fill();
 };
 
@@ -122,7 +122,7 @@ var update = function() {
 };
 
 var render = function() {
-    context.fillStyle = "#FF00FF";
+    context.fillStyle = "#000000";
     context.fillRect(0, 0, width, height);
     player.render();
     computer.render();
